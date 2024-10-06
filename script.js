fef01962244c123fb07ea6d96fb72fa4ed9208a6
hiteshrajput003 <hiteshrohilla2003@gmail.com>
2024-10-06 12:43:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function openSurprisePage() {
+    window.location.href = "surprise.html";
+}
+// function openPDF() {
+//     window.open("./assets/pdf/Hey_you.pdf"); // Replace with your actual PDF path
+// }
